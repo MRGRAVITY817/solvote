e352b2c7c5d89592b0c154d555f6eac86cfcfe40
Hoon Wee <mrgravity817@gmail.com>
2024-03-20 22:43:41 +0900

    feat: connect wallet button

---
+++ contexts/wallet.tsx
@@ -0,0 +1,11 @@
+
+const WalletContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
+
+
+export default WalletContextProvider;
